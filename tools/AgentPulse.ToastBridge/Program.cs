2ad84ae930395acc8d286b18a5fc2e059a9616c8
xuicst <2179238624@qq.com>
2026-07-10 11:25:00 +0800

    testing

---
+++ tools/AgentPulse.ToastBridge/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Windows.AppNotifications;
+using Microsoft.Windows.AppNotifications.Builder;
+
+var title = args.Length > 0 ? args[0] : "AgentPulse";
+var message = args.Length > 1 ? args[1] : "Notification";
+
+AppNotificationManager.Default.Register();
+
+var notification = new AppNotificationBuilder()
+    .AddText(title)
+    .AddText(message)
+    .BuildNotification();
+
+AppNotificationManager.Default.Show(notification);
